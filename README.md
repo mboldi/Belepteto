b22ab96a47000a11c1e93c4f558f5347667839f1
Marta Boldi <marta.boldi13@gmail.com>
2016-10-12 12:09:25 +0000

    aktualis allapot

--- README.md
+++
@@ -1,1 +0,0 @@
-# Belepteto
--- belepteto.py
+++ belepteto.py
@@ -20,7 +20,7 @@ print(port.isOpen())
 print("Com Port opened...")
 
 # CharLCD pin configuration:
-lcd_rs    = 27
+lcd_rs    = 27  
 lcd_en    = 22
 lcd_d4    = 5
 lcd_d5    = 6
@@ -38,31 +38,30 @@ lcd_rows    = 2
 lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                               lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue)
 
-
 def lcdMsg(message, color):
     lcd.clear()
     if color == 'blue':
-        lcd.set_color(0.0, 0.0, 1.0) #blue background color
+	lcd.set_color(0.0, 0.0, 1.0) #blue background color
     elif color == 'green':
-        lcd.set_color(0.0, 1.0, 0.0) #green background color
+	lcd.set_color(0.0, 1.0, 0.0) #green background color
     elif color == 'white':
-        lcd.set_color(1.0, 1.0, 1.0) #white bg color
+	lcd.set_color(1.0, 1.0, 1.0) #white bg color
     elif color == 'none':
-        lcd.set_color(0.0, 0.0, 0.0) #no bg color
+	lcd.set_color(0.0, 0.0, 0.0) #no bg color
     elif color == 'red':
-        lcd.set_color(1.0, 0.0, 0.0) #red bg color
+	lcd.set_color(1.0, 0.0, 0.0) #red bg color
 
     lcd.message(message)
 
     if '\n' not in message:
-        case = len(message) > lcd_columns
+	case = len(message) > lcd_columns
     else:
-        case =  len(message.split('\n')[0]) > lcd_columns or len(message.split('\n')[1]) > lcd_columns
-
+	case =  len(message.split('\n')[0]) > lcd_columns or len(message.split('\n')[1]) > lcd_columns
+	
     if case:
-        for i in range(len(message)-lcd_columns):
-            time.sleep(0.3)
-            lcd.move_left()
+	for i in range(len(message)-lcd_columns):
+	    time.sleep(0.3)
+	    lcd.move_left()
 
 #def commWithServer(cardID):
     #nincs meg meg
@@ -76,19 +75,19 @@ wasRead = False
 while True:
     uid=port.read(16)
 
-    print 'Found card with UID: ' + str(uid)
 
     if uid == '':
-        if wasRead:
-            lcdMsg('Erintsen kartyat\n a leolvasohoz!', 'white')
-            wasRead = False
-        if(time.time()-lastRead) > 60:
-            lcdMsg('Erintsen kartyat\n a leolvasohoz!', 'none')
-        continue
+	if wasRead:
+	    lcdMsg('Erintsen kartyat\n a leolvasohoz!', 'white')
+	    wasRead = False
+	if(time.time()-lastRead) > 60: 
+	    lcdMsg('Erintsen kartyat\n a leolvasohoz!', 'none')
+	continue
     else:
-        lcdMsg('Kartya leolvasva\n' + str(uid), 'green')
-        wasRead = True
+        print 'Found card with UID: ' + str(uid)
+	lcdMsg('Kartya leolvasva\n' + str(uid), 'green')
+	wasRead = True
 
-        #kommunikacio a szerverrel
+	#kommunikacio a szerverrel
 
-        lastRead = time.time()
+	lastRead = time.time()
